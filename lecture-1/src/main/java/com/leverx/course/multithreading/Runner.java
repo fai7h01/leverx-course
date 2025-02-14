778317c6b9963ce02d05eddfa1ae89d1a8d31020
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-14 17:59:45 +0400

    multithreading

--- lecture-1/src/main/java/com/leverx/course/multithreading/Runner.java
+++ lecture-1/src/main/java/com/leverx/course/multithreading/Runner.java
@@ -2,13 +2,22 @@ package com.leverx.course.multithreading;
 
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        MultiThreadClass multiThreadClass1 = new MultiThreadClass();
-        MultiThreadClass multiThreadClass2 = new MultiThreadClass();
+//        MultiThreadClass multiThreadClass1 = new MultiThreadClass();
+//        MultiThreadClass multiThreadClass2 = new MultiThreadClass();
+//
+//        multiThreadClass1.start();
+//        multiThreadClass2.start();
 
-        multiThreadClass1.start();
-        multiThreadClass2.start();
+        for (int i = 0; i < 5; i++) {
+            MultiThreadClass multiThreadClass = new MultiThreadClass(i);
+            Thread thread = new Thread(multiThreadClass);
+            thread.start();
+            //thread.join();
+            //System.out.println(thread.isAlive());
+        }
+
 
     }
 
