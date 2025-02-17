334f54e6ac5e69cb7a8b84d8755094bd5288831e
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-17 16:07:54 +0400

    lecture 2

--- lecture-1/src/main/java/com/leverx/course/Main.java
+++ lecture-1/src/main/java/com/leverx/course/Main.java
@@ -1,7 +1,22 @@
 package com.leverx.course;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println("Hello world!");
+
+        Properties properties = System.getProperties();
+        System.out.println(properties);
+
+        List<String> list = new ArrayList<>(Arrays.asList("1", "2", "3"));
+        Stream<?> stream = Stream.empty();
+        stream = list.stream();
+        list.forEach(System.out::println);
+
     }
 }
