02d36082a73dd7b01f55a959fa97dea1ff735a78
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-18 18:13:10 +0400

    pattern matching

--- lecture-2/src/main/java/com/leverx/course/Main.java
+++ lecture-2/src/main/java/com/leverx/course/Main.java
@@ -8,6 +8,8 @@ import com.leverx.course.sealedclasses.Vehicle;
 public class Main {
     public static void main(String[] args) {
 
+        //note: every permitted subclass must define modifier: final, sealed or non-sealed
+
         Service serviceCar = new Car(4);
         Service serviceTruck = new Truck(15000);
 
@@ -19,6 +21,19 @@ public class Main {
 
         System.out.println(vehicleCar.getRegistrationNumber());
         System.out.println(vehicleTruck.getRegistrationNumber());
+
+        //traditional way
+        if (vehicleTruck instanceof Service) {
+            int maxServiceIntervalInMonths =
+                    ((Service) vehicleTruck).getMaxServiceIntervalInMonths();
+            System.out.println(maxServiceIntervalInMonths);
+        }
+
+        //pattern matching
+        if (vehicleCar instanceof Service service) {
+            int maxServiceIntervalInMonths = service.getMaxServiceIntervalInMonths();
+            System.out.println(maxServiceIntervalInMonths);
+        }
 
     }
 
