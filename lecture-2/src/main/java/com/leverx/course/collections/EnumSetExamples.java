38e3491b13877993ee5ab4081b3790227834eb24
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-20 19:58:32 +0400

    enumSet

---
+++ lecture-2/src/main/java/com/leverx/course/collections/EnumSetExamples.java
@@ -0,0 +1,26 @@
+package com.leverx.course.collections;
+
+import java.util.Currency;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class EnumSetExamples {
+
+    enum Currency {USD, EUR, GEL}
+
+    public static void main(String[] args) {
+
+        Set<Currency> always = EnumSet.allOf(Currency.class);
+        System.out.println(always);
+
+        Set<Currency> never = EnumSet.noneOf(Currency.class);
+        System.out.println(never);
+
+        Set<Currency> global = EnumSet.range(Currency.USD, Currency.EUR);
+        System.out.println(global);
+
+        Set<Currency> ge = EnumSet.of(Currency.GEL, Currency.EUR);
+        System.out.println(ge);
+    }
+
+}
