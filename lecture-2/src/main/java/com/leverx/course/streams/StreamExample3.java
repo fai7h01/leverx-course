51f96135d429990302bec202bf8ae0c931687217
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-23 18:47:13 +0400

    streams

--- lecture-2/src/main/java/com/leverx/course/streams/StreamExample3.java
+++ lecture-2/src/main/java/com/leverx/course/streams/StreamExample3.java
@@ -1,9 +1,8 @@
 package com.leverx.course.streams;
 
-import javax.swing.text.html.Option;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamExample3 {
 
@@ -20,7 +19,19 @@ public class StreamExample3 {
         startsWithA = words.stream().parallel().filter(word -> word.startsWith("a")).findAny();
 
         boolean anyMatch = words.parallelStream().anyMatch(word -> word.startsWith("b"));
-        System.out.println(anyMatch);
+        //System.out.println(anyMatch);
+
+        Stream.of("a", "b", "c", "d").forEach(System.out::println);
+
+        Set<String> set = Stream.of("a", "a", "a", "v").collect(Collectors.toSet());
+        //System.out.println(set);
+
+        Set<String> treeSet = Stream.of("b", "m", "a", "c").collect(Collectors.toCollection(TreeSet::new));
+        //System.out.println(treeSet);
+
+        String joined = Stream.of("a", "b", "c", "d").collect(Collectors.joining(", "));
+        System.out.println(joined);
+
 
     }
 
