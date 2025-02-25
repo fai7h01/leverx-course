be6452a250cb514a8058abb94f6194a270627288
fai7h01 <l.varsimashvil10@gmail.com>
2025-02-25 19:53:54 +0400

    generic method using wild card

---
+++ lecture-3/src/main/java/com/leverx/course/generics/BoundedWildCard.java
@@ -0,0 +1,13 @@
+package com.leverx.course.generics;
+
+import java.util.List;
+
+public class BoundedWildCard {
+
+    public static <T extends AutoCloseable> void closeAll(List<T> elements) throws Exception{
+        for (T element : elements) {
+            element.close();
+        }
+    }
+
+}
